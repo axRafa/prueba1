d8ee6f53cd8b6337ec7fff31093c8dac639b419c
axRafa <rfarfan@estudiantes.unsam.edu.ar>
2023-11-06 03:24:02 -0300

    commit prueba

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from modulos.animales import Mamifero
 
 elefante = Mamifero(6, 80)
 
+perro = Mamifero(8, 20)
+
 print(elefante.cantMamas)
 print(elefante.cantPatas)
 print(elefante.esperanzaDeVida)
